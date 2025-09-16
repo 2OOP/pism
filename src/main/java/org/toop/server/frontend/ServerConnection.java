3afd718d9111c7d54cdf1eda6755018c6ee5d078
Bas de Jong <basdejong2002@live.nl>
2025-09-16 12:54:41 +0200

    fixes

--- src/main/java/org/toop/server/frontend/ServerConnection.java
+++ src/main/java/org/toop/server/frontend/ServerConnection.java
@@ -3,7 +3,7 @@ package org.toop.server.frontend;
 import org.toop.Main;
 import org.toop.eventbus.Events;
 import org.toop.eventbus.GlobalEventBus;
-import org.toop.server.backend.tictactoe.ServerCommand;
+import org.toop.server.backend.tictactoe.TicTacToeServerCommand;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -45,7 +45,7 @@ public final class ServerConnection implements Runnable {
      * @param args The arguments for the command.
      */
     public void sendCommandByString(String command, String... args) {
-        if (!ServerCommand.isValid(command)) {
+        if (!TicTacToeServerCommand.isValid(command)) {
             logger.error("Invalid command: {}", command);
             return;
         }
