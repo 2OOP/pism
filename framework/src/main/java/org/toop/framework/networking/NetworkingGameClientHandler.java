9937c6a7a6a1b277c8e1ffe1729a0463221b12df
lieght <49651652+BAFGdeJong@users.noreply.github.com>
2025-09-28 20:31:26 +0200

    Server in working state, can be merged with working branch.

--- framework/src/main/java/org/toop/framework/networking/NetworkingGameClientHandler.java
+++ framework/src/main/java/org/toop/framework/networking/NetworkingGameClientHandler.java
@@ -2,13 +2,11 @@ package org.toop.framework.networking;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import jdk.jfr.Event;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.toop.framework.eventbus.EventFlow;
 import org.toop.framework.networking.events.NetworkEvents;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -44,23 +42,147 @@ public class NetworkingGameClientHandler extends ChannelInboundHandlerAdapter {
     }
 
     private void parseServerReturn(String rec) {
-        if (rec.toLowerCase().contains("playerlist")) {
-            playerListHandler(rec);
-        } else if (rec.toLowerCase().contains("close")) {
 
-        } else {}
+        String recSrvRemoved = rec.substring("SVR ".length());
+        Pattern gamePattern = Pattern.compile("GAME (\\w+)", Pattern.CASE_INSENSITIVE);
+        Matcher gameMatch = gamePattern.matcher(recSrvRemoved);
+
+        if (gameMatch.find()) {
+            switch(gameMatch.group(1)) {
+                case "YOURTURN":   gameYourTurnHandler(recSrvRemoved);     return;
+                case "MOVE":       gameMoveHandler(recSrvRemoved);         return;
+                case "MATCH":      gameMatchHandler(recSrvRemoved);        return;
+                case "CHALLENGE":  gameChallengeHandler(recSrvRemoved);    return;
+                case "WIN",
+                     "DRAW",
+                     "LOSE":       gameWinConditionHandler(recSrvRemoved); return;
+                default:                                                   return;
+            }
+        } else {
+
+             Pattern getPattern = Pattern.compile("(\\w+)", Pattern.CASE_INSENSITIVE);
+             Matcher getMatch = getPattern.matcher(recSrvRemoved);
+
+            if (getMatch.find()) {
+                switch(getMatch.group(1)) {
+                    case "PLAYERLIST": playerlistHandler(recSrvRemoved); return;
+                    case "GAMELIST":   gamelistHandler(recSrvRemoved);   return;
+                    case "HELP":       helpHandler(recSrvRemoved);       return;
+                    default:                                             return;
+                }
+            } else {
+                return; // TODO: Should be an error.
+            }
+        }
     }
 
-    private void playerListHandler(String rec) {
-            Pattern pattern = Pattern.compile("\"([^\"]+)\"");
-            String[] players = pattern.matcher(rec)
+    private void gameMoveHandler(String rec) {
+        String[] msg = Pattern
+                .compile("(?:player|details|move):\\s*\"?([^\",}]+)\"?", Pattern.CASE_INSENSITIVE)
+                .matcher(rec)
+                .results()
+                .map(m -> m.group(1).trim())
+                .toArray(String[]::new);
+
+        new EventFlow()
+                .addPostEvent(new NetworkEvents.GameMoveResponse(this.connectionId, msg[0], msg[1], msg[2]))
+                .asyncPostEvent();
+    }
+
+    private void gameWinConditionHandler(String rec) {
+        String condition = Pattern
+                .compile("\\b(win|draw|lose)\\b", Pattern.CASE_INSENSITIVE)
+                .matcher(rec)
+                .results()
+                .toString()
+                .trim();
+
+        new EventFlow()
+                .addPostEvent(new NetworkEvents.GameResultResponse(this.connectionId, condition))
+                .asyncPostEvent();
+    }
+
+    private void gameChallengeHandler(String rec) {
+        boolean isCancelled = rec.toLowerCase().startsWith("challenge accepted");
+        try {
+            String[] msg = Pattern
+                    .compile("(?:CHALLENGER|GAMETYPE|CHALLENGENUMBER):\\s*\"?(.*?)\"?\\s*(?:,|})")
+                    .matcher(rec)
                     .results()
-                    .map(m -> m.group(1))
+                    .map(m -> m.group().trim())
                     .toArray(String[]::new);
 
-            new EventFlow()
-                    .addPostEvent(new NetworkEvents.PlayerListResponse(this.connectionId, players))
+            if (isCancelled) new EventFlow()
+                    .addPostEvent(new NetworkEvents.ChallengeCancelledResponse(this.connectionId, msg[0]))
                     .asyncPostEvent();
+            else             new EventFlow()
+                    .addPostEvent(new NetworkEvents.ChallengeResponse(this.connectionId, msg[0], msg[1], msg[2]))
+                    .asyncPostEvent();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.error("Array out of bounds for: {}", rec, e);
+        }
+    }
+
+    private void gameMatchHandler(String rec) {
+        try {
+            String[] msg = Pattern
+                    .compile("\"([^\"]*)\"")
+                    .matcher(rec)
+                    .results()
+                    .map(m -> m.group(1).trim())
+                    .toArray(String[]::new);
+
+            // [0] playerToMove, [1] gameType, [2] opponent
+            new EventFlow()
+                    .addPostEvent(new NetworkEvents.GameMatchResponse(this.connectionId, msg[0], msg[1], msg[2]))
+                    .asyncPostEvent();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.error("Array out of bounds for: {}", rec, e);
+        }
+
+    }
+
+    private void gameYourTurnHandler(String rec) {
+        String msg = Pattern
+                .compile("TURNMESSAGE:\\s*\"([^\"]*)\"")
+                .matcher(rec)
+                .results()
+                .toString()
+                .trim();
+
+        new EventFlow()
+                .addPostEvent(new NetworkEvents.YourTurnResponse(this.connectionId, msg))
+                .asyncPostEvent();
+    }
+
+    private void playerlistHandler(String rec) {
+        String[] players = Pattern
+                .compile("\"([^\"]+)\"")
+                .matcher(rec)
+                .results()
+                .map(m -> m.group(1).trim())
+                .toArray(String[]::new);
+
+        new EventFlow()
+                .addPostEvent(new NetworkEvents.PlayerlistResponse(this.connectionId, players))
+                .asyncPostEvent();
+    }
+
+    private void gamelistHandler(String rec) {
+        String[] gameTypes = Pattern
+                .compile("\"([^\"]+)\"")
+                .matcher(rec)
+                .results()
+                .map(m -> m.group(1).trim())
+                .toArray(String[]::new);
+
+        new EventFlow()
+            .addPostEvent(new NetworkEvents.GamelistResponse(this.connectionId, gameTypes))
+            .asyncPostEvent();
+    }
+
+    private void helpHandler(String rec) {
+        logger.info(rec);
     }
 
     @Override
