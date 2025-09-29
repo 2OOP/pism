1ec6b7d582de2ce1308a90b265d2c0cb79c6314e
Bas de Jong <basdejong2002@live.nl>
2025-09-29 13:52:02 +0200

    Updated main to be generic.

--- app/src/main/java/org/toop/Main.java
+++ app/src/main/java/org/toop/Main.java
@@ -5,12 +5,12 @@ import org.toop.framework.networking.NetworkingClientManager;
 import org.toop.framework.networking.NetworkingInitializationException;
 
 public class Main {
-	static void main(String[] args) {
-		initSystems();
-		javax.swing.SwingUtilities.invokeLater(LocalServerSelector::new);
-	}
+    static void main(String[] args) {
+        initSystems();
+        javax.swing.SwingUtilities.invokeLater(LocalServerSelector::new);
+    }
 
-	private static void initSystems() throws NetworkingInitializationException {
-		new NetworkingClientManager();
-	}
+    private static void initSystems() throws NetworkingInitializationException {
+        new NetworkingClientManager();
+    }
 }
