f759a9ffb73ef052d8c9ed8e8b22184852622047
Stef <stbuwalda@gmail.com>
2025-09-30 20:23:20 +0200

    Successfully plays multiple instances of the same sound. Returns the clipID so the sound can be stopped.

--- framework/src/main/java/org/toop/framework/assets/resources/AudioResource.java
+++ framework/src/main/java/org/toop/framework/assets/resources/AudioResource.java
@@ -9,7 +9,10 @@ public class AudioResource extends Resource {
     }
 
     public Clip getNewClip() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        // Get a new clip from audio system
         Clip clip = AudioSystem.getClip();
+
+        // Insert a new audio stream into the clip
         clip.open(this.getAudioStream());
         return clip;
     }
--- framework/src/main/java/org/toop/framework/audio/SoundManager.java
+++ framework/src/main/java/org/toop/framework/audio/SoundManager.java
@@ -12,8 +12,7 @@ import java.util.*;
 import javax.sound.sampled.*;
 
 public class SoundManager {
-    private final Map<String, Clip> activeClips = new HashMap<>();
-    private final HashMap<String, Clip> clips = new HashMap<>();
+    private final Map<Long, Clip> activeClips = new HashMap<>();
     private final HashMap<String, AudioResource> audioResources = new HashMap<>();
     private final SnowflakeGenerator idGenerator = new SnowflakeGenerator(); // TODO: Don't create a new generator
 
@@ -21,7 +20,7 @@ public class SoundManager {
         // Get all Audio Resources and add them to a list.
         for (Asset<AudioResource> resource : asm.getAllResourceOfType(AudioResource.class).values()) {
             try {
-                addClip(resource);
+                this.addAudioResource(resource);
             } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
                 throw new RuntimeException(e);
             }
@@ -36,23 +35,22 @@ public class SoundManager {
     }
 
     private void handleStopSound(AudioEvents.StopAudio event) {
-        this.stopSound(event.fileNameNoExtensionAndNoDirectory());
+        this.stopSound(event.clipId());
     }
 
-    private void addClip(Asset<AudioResource> audioAsset)
+    private void addAudioResource(Asset<AudioResource> audioAsset)
             throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         AudioResource audioResource = audioAsset.getResource();
 
-        this.clips.put(audioAsset.getName(), audioResource.getNewClip());
         this.audioResources.put(audioAsset.getName(), audioResource);
     }
 
-    private void playSound(String audioFileName, boolean loop) {
+    private long playSound(String audioFileName, boolean loop) {
         try {
             AudioResource resource = audioResources.get(audioFileName);
 
             if (resource == null){
-                return;
+                return -1;
             }
 
             Clip clip = resource.getNewClip();
@@ -65,27 +63,28 @@ public class SoundManager {
                 clip.start();
             }
 
+            // Generate id for clip
+            long clipId = idGenerator.nextId();
+
             // store it so we can stop it later
-            activeClips.put(audioFileName, clip); // TODO: Do on snowflake for specific sound to stop
+            activeClips.put(clipId, clip); // TODO: Do on snowflake for specific sound to stop
 
             // remove when finished (only for non-looping sounds)
             clip.addLineListener(event -> {
                 if (event.getType() == LineEvent.Type.STOP && !clip.isRunning()) {
-                    activeClips.remove(audioFileName);
+                    activeClips.remove(clipId);
                     clip.close();
                 }
             });
+
+            return clipId;
         } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public HashMap<String, Clip> getClips() {
-        return this.clips;
-    }
-
-    public void stopSound(String audioFileName) {
-        Clip clip = activeClips.get(audioFileName);
+    public void stopSound(long clipId) {
+        Clip clip = activeClips.get(clipId);
 
         if (clip == null) {
             return;
@@ -93,7 +92,7 @@ public class SoundManager {
 
         clip.stop();
         clip.close();
-        activeClips.remove(audioFileName);
+        activeClips.remove(clipId);
     }
 
     public void stopAllSounds() {
