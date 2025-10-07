58269e917ac0866bd34ea676b3038a38d4728f70
Stef <48526421+StefBuwalda@users.noreply.github.com>
2025-10-07 09:45:59 +0200

    Split AudioVolumeManager from SoundManager (#134)

---
+++ framework/src/main/java/org/toop/framework/audio/AudioVolumeManager.java
@@ -0,0 +1,69 @@
+package org.toop.framework.audio;
+
+import javafx.scene.media.MediaPlayer;
+import org.toop.framework.audio.events.AudioEvents;
+import org.toop.framework.eventbus.EventFlow;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+public class AudioVolumeManager {
+    private final SoundManager sM;
+
+    private double volume = 1.0;
+    private double fxVolume = 1.0;
+
+    public AudioVolumeManager(SoundManager soundManager){
+        this.sM = soundManager;
+
+        new EventFlow()
+                .listen(this::handleVolumeChange)
+                .listen(this::handleFxVolumeChange)
+                .listen(this::handleGetCurrentVolume)
+                .listen(this::handleGetCurrentFxVolume);
+    }
+
+    public void updateMusicVolume(MediaPlayer mediaPlayer){
+        mediaPlayer.setVolume(this.volume);
+    }
+
+    public void updateSoundEffectVolume(Clip clip){
+        if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)){
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float min = volumeControl.getMinimum();
+            float max = volumeControl.getMaximum();
+            float dB = (float) (Math.log10(Math.max(fxVolume, 0.0001)) * 20.0); // convert linear to dB
+            dB = Math.max(min, Math.min(max, dB));
+            volumeControl.setValue(dB);
+        }
+    }
+
+    private double limitVolume(double volume) {
+        if (volume > 1.0) return 1.0;
+        else return Math.max(volume, 0.0);
+    }
+
+    private void handleFxVolumeChange(AudioEvents.ChangeFxVolume event) {
+        this.fxVolume = limitVolume(event.newVolume() / 100);
+        for (Clip clip : sM.getActiveSoundEffects().values()){
+            updateSoundEffectVolume(clip);
+        }
+    }
+
+    private void handleVolumeChange(AudioEvents.ChangeVolume event) {
+        this.volume = limitVolume(event.newVolume() / 100);
+        for (MediaPlayer mediaPlayer : sM.getActiveMusic()) {
+            this.updateMusicVolume(mediaPlayer);
+        }
+    }
+
+    private void handleGetCurrentVolume(AudioEvents.GetCurrentVolume event) {
+        new EventFlow().addPostEvent(new AudioEvents.GetCurrentVolumeResponse(volume * 100, event.snowflakeId()))
+                .asyncPostEvent();
+    }
+
+    private void handleGetCurrentFxVolume(AudioEvents.GetCurrentFxVolume event) {
+        new EventFlow().addPostEvent(new AudioEvents.GetCurrentFxVolumeResponse(fxVolume * 100, event.snowflakeId()))
+                .asyncPostEvent();
+    }
+}
