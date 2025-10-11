d958b9730abd335d55604ff73bc50aa26c99067f
Stef <48526421+StefBuwalda@users.noreply.github.com>
2025-10-11 23:00:06 +0200

    Split SoundEffectManager from AudioManager. (#171)
    Clips no longer create a new clip instance each time they are played.  A singular clip is made for each resource and is opened/closed when loaded/unloaded. When a clip is played that is already playing it'll stop playback and start again. Clip volume handling isn't done very well.

--- framework/src/main/java/org/toop/framework/audio/SoundEffectManager.java
+++ framework/src/main/java/org/toop/framework/audio/SoundEffectManager.java
@@ -1,27 +1,64 @@
 package org.toop.framework.audio;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.toop.framework.resource.ResourceManager;
+import org.toop.framework.resource.ResourceMeta;
+import org.toop.framework.resource.resources.BaseResource;
 import org.toop.framework.resource.resources.SoundEffectAsset;
 
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SoundEffectManager implements org.toop.framework.audio.interfaces.SoundEffectManager<SoundEffectAsset> {
-    private final Map<Long, SoundEffectAsset> activeSoundEffects = new HashMap<>();
-    private final HashMap<String, SoundEffectAsset> audioResources = new HashMap<>();
+    private static final Logger logger = LogManager.getLogger(SoundEffectManager.class);
+    private final HashMap<String, SoundEffectAsset> soundEffectResources;
+
+    public SoundEffectManager(){
+        // If there are duplicates, takes discards the first
+        soundEffectResources = ResourceManager.getAllOfType(SoundEffectAsset.class).stream()
+                .collect(Collectors.toMap(ResourceMeta::getName, ResourceMeta::getResource, (a, b) -> b, HashMap::new));
+
+    }
 
     @Override
     public Collection<SoundEffectAsset> getActiveAudio() {
-        return this.audioResources.values();
+        return this.soundEffectResources.values();
     }
 
     @Override
     public void play(String name, boolean loop) {
-        //TODO
+        SoundEffectAsset asset = soundEffectResources.get(name);
+
+        if (asset == null) {
+            logger.warn("Unable to load audio asset: {}", name);
+            return;
+        }
+
+        asset.play();
+        // TODO: Volume of Sound Effect isn't set when loading. When loading an effect it will be full volume.
+        logger.debug("Playing sound: {}", asset.getFile().getName());
     }
 
     @Override
-    public void stop(long clipId) {
-        //TODO
+    public void stop(String name){
+        SoundEffectAsset asset = soundEffectResources.get(name);
+
+        if (asset == null) {
+            logger.warn("Unable to load audio asset: {}", name);
+            return;
+        }
+
+        asset.stop();
+
+        logger.debug("Stopped sound: {}", asset.getFile().getName());
     }
 }
