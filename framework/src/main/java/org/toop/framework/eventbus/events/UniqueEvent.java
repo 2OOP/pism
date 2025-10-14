14e6785c6f0b2698ffda76267bfed6efb6651b6f
lieght <49651652+BAFGdeJong@users.noreply.github.com>
2025-10-15 00:01:41 +0200

    Some better docs.

--- framework/src/main/java/org/toop/framework/eventbus/events/UniqueEvent.java
+++ framework/src/main/java/org/toop/framework/eventbus/events/UniqueEvent.java
@@ -1,5 +1,16 @@
 package org.toop.framework.eventbus.events;
 
+/**
+ * MUST HAVE long identifier at the end.
+ * e.g.
+ *
+ * <pre>{@code
+ * public record uniqueEvent(String content, long identifier) implements UniqueEvent {};
+ * public record uniqueEvent(long identifier) implements UniqueEvent {};
+ * public record uniqueEvent(String content, int number, long identifier) implements UniqueEvent {};
+ * }</pre>
+ *
+ */
 public interface UniqueEvent extends EventType {
     default long getIdentifier() {
         try {
