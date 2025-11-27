0cb025edb9c6a339a528cb22e285f0d94dafc00e
Stef <stbuwalda@gmail.com>
2025-11-27 18:37:34 +0100

    Changed the way turns are being stored in TurnBasedGame.

--- game/src/main/java/org/toop/game/TurnBasedGame.java
+++ game/src/main/java/org/toop/game/TurnBasedGame.java
@@ -1,26 +1,25 @@
 package org.toop.game;
 
 public abstract class TurnBasedGame extends Game {
-    private final int turns;
-    private int currentTurn;
+    private final int playerCount;  // How many players are playing
+    private int turn = 0;           // What turn it is in the game
 
-    protected TurnBasedGame(int rowSize, int columnSize, int turns) {
+    protected TurnBasedGame(int rowSize, int columnSize, int playerCount) {
         super(rowSize, columnSize);
-        assert turns >= 2;
-        this.turns = turns;
+        this.playerCount = playerCount;
     }
 
     protected TurnBasedGame(TurnBasedGame other) {
         super(other);
-        turns = other.turns;
-        currentTurn = other.currentTurn;
+        playerCount = other.playerCount;
+        turn = other.turn;
     }
 
     protected void nextTurn() {
-        currentTurn = (currentTurn + 1) % turns;
+        turn += 1;
     }
 
     public int getCurrentTurn() {
-        return currentTurn;
+        return turn % playerCount;
     }
 }
