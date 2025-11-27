40ddf08e2d09c877a915ef5f2e4902e96f5cd7ed
Stef <stbuwalda@gmail.com>
2025-11-27 16:58:51 +0100

    Revert "Merge remote-tracking branch 'origin/Development' into Development"
    This reverts commit 59d46cb73c3bf208ee359ce86f3ce4306d4e5964, reversing
    changes made to 38681c5db03e3e170c90ec3ec6795b6331b157df.

--- game/src/main/java/org/toop/game/reversi/ReversiAI.java
+++ game/src/main/java/org/toop/game/reversi/ReversiAI.java
@@ -8,7 +8,6 @@ public final class ReversiAI extends AI<Reversi> {
 	public Move findBestMove(Reversi game, int depth) {
         Move[] moves = game.getLegalMoves();
         int inty = (int)(Math.random() * moves.length-.5f);
-        if (moves.length == 0) return null;
 		return moves[inty];
 	}
 }
