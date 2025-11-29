0746136ec79e6cd9140d1d0224466ffdb83bd0a4
Stef <stbuwalda@gmail.com>
2025-11-29 01:01:12 +0100

    Merge conflict fix

--- app/src/main/java/org/toop/app/game/GameController.java
+++ app/src/main/java/org/toop/app/game/GameController.java
@@ -6,7 +6,7 @@ import org.toop.app.widget.view.GameView;
 
 public abstract class GameController implements UpdatesGameUI {
     // Reference to primary view
-    protected final GameView primary = new GameView(null, null, null);
+    protected final GameView primary;
 
     // Reference to game canvas
     protected final GameCanvas canvas;
@@ -14,8 +14,10 @@ public abstract class GameController implements UpdatesGameUI {
     // Reference to gameThread
     protected TurnBasedGameThread gameThread;
 
-    protected GameController(GameCanvas canvas) {
+    // TODO: Change gameType to automatically happen with either dependency injection or something else.
+    protected GameController(GameCanvas canvas, String gameType) {
         this.canvas = canvas;
+        primary = new GameView(null, null, null, gameType);
     }
 
     protected void setThread(TurnBasedGameThread gameThread){
