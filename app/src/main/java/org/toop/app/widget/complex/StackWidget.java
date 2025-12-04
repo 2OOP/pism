0d564283fb9620535670e7b23a3a03f6f615ccbd
ramollia <>
2025-12-04 21:52:22 +0100

    fixed redundant container

--- app/src/main/java/org/toop/app/widget/complex/StackWidget.java
+++ app/src/main/java/org/toop/app/widget/complex/StackWidget.java
@@ -8,21 +8,18 @@ import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 
 public abstract class StackWidget extends StackPane implements Widget {
-    private final StackPane container;
-
 	public StackWidget(String cssClass) {
-        container = new StackPane();
-		container.getStyleClass().add(cssClass);
+		this.getStyleClass().add(cssClass);
 	}
 
 	public void add(Pos position, Node node) {
 		Platform.runLater(() -> {
-			if (container.getChildren().contains(node)) {
+			if (this.getChildren().contains(node)) {
 				return;
 			}
 
 			StackPane.setAlignment(node, position);
-			container.getChildren().add(node);
+			this.getChildren().add(node);
 		});
 	}
 
@@ -32,7 +29,7 @@ public abstract class StackWidget extends StackPane implements Widget {
 
 	public void remove(Node node) {
 		Platform.runLater(() -> {
-			container.getChildren().remove(node);
+			this.getChildren().remove(node);
 		});
 	}
 
@@ -42,6 +39,6 @@ public abstract class StackWidget extends StackPane implements Widget {
 
 	@Override
 	public StackPane getNode() {
-		return container;
+		return this;
 	}
 }
