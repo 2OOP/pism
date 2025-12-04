75af655b49773e2115f3c67dda1d6fb4ff474aae
lieght <49651652+BAFGdeJong@users.noreply.github.com>
2025-12-04 16:51:32 +0100

    Changed to debug instead of info

--- app/src/main/java/org/toop/app/widget/Widget.java
+++ app/src/main/java/org/toop/app/widget/Widget.java
@@ -2,19 +2,28 @@ package org.toop.app.widget;
 
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.toop.framework.resource.ResourceLoader;
 
 public interface Widget {
+    Logger logger = LogManager.getLogger(Widget.class);
+
 	Node getNode();
 
     default void show(Pos position) {
+        logger.debug("Showing Widget: {} at position: {}", this.getNode(), position.toString());
         WidgetContainer.add(position, this);
     }
 
     default void hide() {
+        logger.debug("Hiding Widget: {}", this.getNode());
         WidgetContainer.remove(this);
     }
 
     default void replace(Pos position, Widget widget) {
+        logger.debug("Replacing Widget: {}, with widget: {}, to position: {}",
+                this.getNode(), widget.getNode(), position.toString());
         widget.show(position);
 		hide();
     }
