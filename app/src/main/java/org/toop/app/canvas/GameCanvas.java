6e6a383708d8df85c2b8c5b0fc10242996dc6a4f
Stef <stbuwalda@gmail.com>
2026-01-07 13:26:43 +0100

    Collapsed interfaces in View section

--- app/src/main/java/org/toop/app/canvas/GameCanvas.java
+++ app/src/main/java/org/toop/app/canvas/GameCanvas.java
@@ -1,7 +1,9 @@
 package org.toop.app.canvas;
 
 import javafx.scene.canvas.Canvas;
+import org.toop.framework.gameFramework.model.game.TurnBasedGame;
 
-public interface GameCanvas extends GameDrawer{
+public interface GameCanvas {
     Canvas getCanvas();
+    void redraw(TurnBasedGame gameCopy);
 }
--- app/src/main/java/org/toop/app/canvas/GameDrawer.java
+++
@@ -1,7 +0,0 @@
-package org.toop.app.canvas;
-
-import org.toop.framework.gameFramework.model.game.TurnBasedGame;
-
-public interface GameDrawer {
-    void redraw(TurnBasedGame gameCopy);
-}
