22a73fc50a86b443cc17c112af37d941fdd2638b
lieght <49651652+BAFGdeJong@users.noreply.github.com>
2026-01-23 19:16:29 +0100

    Moved back in threads

--- game/src/test/java/research/AITest.java
+++ game/src/test/java/research/AITest.java
@@ -42,8 +42,8 @@ public class AITest {
         var versions = new ArtificialPlayer[4];
         versions[0] = new ArtificialPlayer(new MCTSAI1(10), "MCTS V1");
         versions[1] = new ArtificialPlayer(new MCTSAI2(10), "MCTS V2");
-        versions[2] = new ArtificialPlayer(new MCTSAI3(10), "MCTS V3");
-        versions[3] = new ArtificialPlayer(new MCTSAI4(10), "MCTS V4");
+        versions[2] = new ArtificialPlayer(new MCTSAI3(10, 8), "MCTS V3");
+        versions[3] = new ArtificialPlayer(new MCTSAI4(10, 8), "MCTS V4");
 
         for (int i = 0; i < versions.length; i++) {
             for (int j = i + 1; j < versions.length; j++) {
